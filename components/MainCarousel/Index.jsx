5d8d1289d16c5c979b742d6314917b3619b1e075
Hernan <hernan.r9317@gmail.com>
2022-09-06 18:07:16 -0300

    fix carousel images

--- components/MainCarousel/Index.jsx
+++ components/MainCarousel/Index.jsx
@@ -6,19 +6,19 @@ import slider3 from '../../public/assets/carousel/img2.png';
 
 export const MainCarousel = () => {
   return (
-    <Carousel id="crouselBoostrap">
+    <Carousel id="mainCarousel">
       <Carousel.Item>
-        <Image src={slider1} alt="First slide" height="1000px" />
+        <Image src={slider1} alt="First slide" />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <Image src={slider2} alt="Second slide" height="1000px" />
+        <Image src={slider2} alt="Second slide" />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
 
       <Carousel.Item>
-        <Image src={slider3} alt="Third slide" height="1000px" />
+        <Image src={slider3} alt="Third slide" />
         <Carousel.Caption></Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- components/TipsCarousel/index.jsx
+++ components/TipsCarousel/index.jsx
@@ -1,30 +1,26 @@
 import Image from 'next/image';
-import {Carousel, Container} from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap';
 import slider1 from '../../public/assets/carousel/contraseña.png';
 import slider2 from '../../public/assets/carousel/escritorio.png';
 import slider3 from '../../public/assets/carousel/navegacion.png';
 
 export const TipsCarousel = () => {
   return (
-    <Container
-      style={{marginLeft: 'auto', marginRight: 'auto', marginTop: '50px'}}
-    >
-      <Carousel variant="dark">
-        <Carousel.Item>
-          <Image src={slider1} alt="First slide" />
-          <Carousel.Caption></Carousel.Caption>
-        </Carousel.Item>
+    <Carousel variant="dark" class="mt-1 mb-5">
+      <Carousel.Item>
+        <Image src={slider1} alt="First slide" />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
 
-        <Carousel.Item>
-          <Image src={slider2} alt="Second slide" />
-          <Carousel.Caption></Carousel.Caption>
-        </Carousel.Item>
+      <Carousel.Item>
+        <Image src={slider2} alt="Second slide" />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
 
-        <Carousel.Item>
-          <Image src={slider3} alt="Third slide" />
-          <Carousel.Caption></Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </Container>
+      <Carousel.Item>
+        <Image src={slider3} alt="Third slide" />
+        <Carousel.Caption></Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 };
