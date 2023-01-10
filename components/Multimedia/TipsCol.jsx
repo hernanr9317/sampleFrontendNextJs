c64addecb66420baebe6a7feb39433fcfc912df7
hernanr9317 <78802962+hernanr9317@users.noreply.github.com>
2023-01-10 11:40:37 -0300

    fix link style card

--- components/Multimedia/TipsCol.jsx
+++ components/Multimedia/TipsCol.jsx
@@ -1,13 +1,24 @@
 import Image from 'next/image';
 import {Col} from 'react-bootstrap';
+import LinesEllipsis from 'react-lines-ellipsis'
+
 
 export const TipsCol = ({data}) => {
   const {title, src, description, link, link2, link3, link4} = data;
+
+ const handleReflow = (rleState) => {
+    const {
+      clamped,
+      text,
+    } = rleState
+    console.log(clamped, text)
+  }
 
   return (
     <Col>
       <div
         style={{
+          // height: '400px',
           display: 'flex',
           justifyContent: 'center',
         }}
@@ -19,21 +30,28 @@ export const TipsCol = ({data}) => {
             alt="Card image cap"
             fill="responsive"
           />
-          <div className="card-body pb-0">
+          <div className="card-body" >
             <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-          </div>
-          <div className="card-body">
-            <a href={link?.href} className="card-link" target="_blank">
+            {/* <p className="card-text">{description}</p> */}
+            <LinesEllipsis
+              className="card-text pb-3"
+              text={description}
+              maxLine='2'
+              ellipsis=' ...'
+              trimRight
+              basedOn='letters'
+              onReflow={ (e) => handleReflow(e)}
+          />
+            <a href={link?.href} className="card-link" target="_blank" >
               {link?.name}
             </a>
-            <a href={link2?.href} className="card-link" target="_blank">
+            <a href={link2?.href} className="card-link" target="_blank" >
               {link2?.name}
             </a>
-            <a href={link3?.href} className="card-link" target="_blank">
+            <a href={link3?.href} className="card-link" target="_blank" >
               {link3?.name}
             </a>
-            <a href={link4?.href} className="card-link" target="_blank">
+            <a href={link4?.href} className="card-link" target="_blank" >
               {link4?.name}
             </a>
           </div>
