9353f8ff6b52cec73d0107fdfbf7a9725073079b
Hernan <hernan.r9317@gmail.com>
2023-01-11 10:13:21 -0300

    add title modal

--- components/Admin/ModalElement.jsx
+++ components/Admin/ModalElement.jsx
@@ -5,36 +5,27 @@ import Modal from 'react-bootstrap/Modal';
 
 export const ModalElement = ({element, interaction}) => {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
-
   const handleShow = () => setShow(true);
 
   useEffect(() => {
     if (element !== '' || undefined) handleShow();
   }, [element, interaction]);
-
+  console.log(element);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>{element.nombre}</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Group className="mb-3" controlId="nombre">
               <Form.Label>Email address</Form.Label>
-              <Form.Control
-                type="email"
-                placeholder="name@example.com"
-                autoFocus
-              />
+              <Form.Control autoFocus />
             </Form.Group>
-            <Form.Group
-              className="mb-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Example textarea</Form.Label>
+            <Form.Group className="mb-3" controlId="descrripcion">
+              <Form.Label>Descripción</Form.Label>
               <Form.Control as="textarea" rows={3} />
             </Form.Group>
           </Form>
