49625665d2941e1c3e60f29737fda6ec2a54adf6
Hernan <hernan.r9317@gmail.com>
2023-01-13 11:51:26 -0300

    initial form modal

--- components/Admin/ModalElement.jsx
+++ components/Admin/ModalElement.jsx
@@ -1,12 +1,23 @@
 import React, {useState, useEffect} from 'react';
+import {useForm} from 'react-hook-form';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Modal from 'react-bootstrap/Modal';
 import {useGetData} from '../../hooks/useGetData';
 
 export const ModalElement = ({element, interaction}) => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
   const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
+  const [edit, setEdit] = useState(true);
+  const handleClose = () => {
+    setShow(false);
+    setEdit(true);
+  };
   const handleShow = () => setShow(true);
 
   useEffect(() => {
@@ -15,23 +26,43 @@ export const ModalElement = ({element, interaction}) => {
 
   const categorias = useGetData('/categorias/');
 
+  const editInfo = () => {
+    setEdit(!edit);
+  };
+
+  const onSaveChanges = (data) => {
+    console.log(data);
+  };
+
   return (
     <>
       <Modal show={show} onHide={handleClose} backdrop="static" size="lg">
         <Modal.Header closeButton>
           <Modal.Title>{element?.nombre}</Modal.Title>
         </Modal.Header>
+        <Button variant="primary" size="sm" onClick={editInfo}>
+          Editar
+        </Button>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit(onSaveChanges)}>
             <Form.Group className="mb-3" controlId="nombre">
               <Form.Label>Nombre</Form.Label>
-              <Form.Control defaultValue={element?.nombre} autoFocus disabled />
+              <Form.Control
+                type="nombre"
+                defaultValue={element?.nombre}
+                autoFocus
+                disabled={edit}
+              />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
               <Form.Group className="mb-3">
                 <Form.Label>Categoria</Form.Label>
-                <Form.Select defaultValue={element?.categoria?.nombre} disabled>
+                <Form.Select
+                  type="categoria"
+                  defaultValue={element?.categoria?.nombre}
+                  disabled={edit}
+                >
                   {categorias?.data?.categorias?.map((catElement, index) => (
                     <option key={index} value={catElement?.nombre}>
                       {catElement?.nombre}
@@ -47,23 +78,24 @@ export const ModalElement = ({element, interaction}) => {
                 defaultValue={element?.precio}
                 type="number"
                 min="0"
-                disabled
+                disabled={edit}
               />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="descrripcion">
               <Form.Label>Descripción</Form.Label>
               <Form.Control
+                type="descripcion"
                 defaultValue={element?.description}
                 as="textarea"
                 rows={5}
-                disabled
+                disabled={edit}
               />
             </Form.Group>
 
             <Form.Group controlId="formFile" className="mb-3">
               <Form.Label>Subir archivo</Form.Label>
-              <Form.Control type="file" disabled />
+              <Form.Control type="file" disabled={edit} />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -71,7 +103,7 @@ export const ModalElement = ({element, interaction}) => {
           <Button variant="secondary" onClick={handleClose}>
             Cerrar
           </Button>
-          <Button variant="primary" onClick={handleClose}>
+          <Button variant="primary" type="submit">
             Gaurdar cambios
           </Button>
         </Modal.Footer>
