f15c8a6715d7e4798d32de6447d938ea21ac6506
Hernan <hernan.r9317@gmail.com>
2023-01-31 15:49:44 -0300

    first tooltip button

--- components/Admin/ModalElement/ModalFooter.jsx
+++ components/Admin/ModalElement/ModalFooter.jsx
@@ -1,12 +1,20 @@
-import {Button, Modal} from 'react-bootstrap';
+import {Button, Modal, OverlayTrigger, Tooltip} from 'react-bootstrap';
 import {FaTrashAlt, FaSave, FaEdit} from 'react-icons/fa';
 
 export const ModalFooter = ({editInfo, edit, handleDelete}) => {
   return (
     <Modal.Footer>
-      <Button variant="primary" style={{display: 'flex'}} onClick={editInfo}>
-        Editar <FaEdit size="20px" style={{marginLeft: '3px'}} />
-      </Button>
+      <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Editar</Tooltip>}>
+        <span className="d-inline-block">
+          <Button
+            variant="primary"
+            style={{display: 'flex'}}
+            onClick={editInfo}
+          >
+            Editar <FaEdit size="20px" style={{marginLeft: '3px'}} />
+          </Button>
+        </span>
+      </OverlayTrigger>
       <Button
         variant="primary"
         type="submit"
