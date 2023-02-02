d1678595b3673808e1425481e2625583fb242198
Hernan <hernan.r9317@gmail.com>
2023-02-02 20:18:02 -0300

    add button avatar

--- components/Admin/MainProfile/DataBox.jsx
+++ components/Admin/MainProfile/DataBox.jsx
@@ -1,24 +1,36 @@
+import {useContext} from 'react';
+import {AuthContext} from './../../../context/auth/AuthContext';
+
 export const DataBox = () => {
+  const data = useContext(AuthContext);
+
   return (
-    <div class="col-md-8">
-      <div class="card mb-3">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-sm-3">
-              <h6 class="mb-0">Nombre completo</h6>
+    <div className="col-md-8">
+      <div className="card mb-3">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-sm-3">
+              <h6 className="mb-0">Nombre completo</h6>
             </div>
-            <div class="col-sm-9 text-secondary">Kenneth Valdez</div>
+            <div className="col-sm-9 text-secondary">{data.user.nombre}</div>
           </div>
           <hr />
-          <div class="row">
-            <div class="col-sm-3">
-              <h6 class="mb-0">Email</h6>
+          <div className="row">
+            <div className="col-sm-3">
+              <h6 className="mb-0">Email</h6>
             </div>
-            <div class="col-sm-9 text-secondary">fip@jukmuh.al</div>
+            <div className="col-sm-9 text-secondary">{data.user.correo}</div>
           </div>
           <hr />
-          <div class="row">
-            <div class="col-sm-12">
+          <div className="row">
+            <div className="col-sm-3">
+              <h6 className="mb-0">Contraseña</h6>
+            </div>
+            <div className="col-sm-9 text-secondary">******</div>
+          </div>
+          <hr />
+          <div className="row">
+            <div className="col-sm-12">
               <button className="btn btn-primary">Editar</button>
             </div>
           </div>
--- components/Admin/MainProfile/ImageBox.jsx
+++ components/Admin/MainProfile/ImageBox.jsx
@@ -1,18 +1,23 @@
+import {useContext} from 'react';
+import {AuthContext} from './../../../context/auth/AuthContext';
+
 export const ImageBox = () => {
+  const data = useContext(AuthContext);
+
   return (
     <div className="card">
       <div className="card-body">
         <div className="d-flex flex-column align-items-center text-center">
-          <img
-            src="https://bootdey.com/img/Content/avatar/avatar7.png"
-            alt="Admin"
-            class="rounded-circle"
-            width="150"
-          />
+          <button className="rounded-circle">
+            <img
+              src="https://bootdey.com/img/Content/avatar/avatar7.png"
+              alt="Admin"
+              className="rounded-circle"
+              width="150"
+            />
+          </button>
           <div className="mt-3">
-            <h4>John Doe</h4>
-            <p className="text-secondary mb-1">Full Stack Developer</p>
-            <button className="btn btn-primary">Cambiar foto</button>
+            <h4>{data?.user?.nombre}</h4>
           </div>
         </div>
       </div>
