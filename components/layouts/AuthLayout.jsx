b54e1c9b702b58343406702e4e8475946a491544
Hernan <hernan.r9317@gmail.com>
2023-02-03 15:19:34 -0300

    fix error login redirect

--- components/layouts/AuthLayout.jsx
+++ components/layouts/AuthLayout.jsx
@@ -1,42 +1,41 @@
 import Head from 'next/head';
-import {useContext, useEffect} from 'react';
+import {useContext, useEffect, useState} from 'react';
 import {AuthContext} from '../../context';
 import {useRouter} from 'next/router';
-import {useState} from 'react';
 
 export const AuthLayout = ({children, title}) => {
   const router = useRouter();
   const {isLoggedIn} = useContext(AuthContext);
-  const [redirectPage, setRedirectPage] = useState(false);
+  const [calledReplace, setCalledReplace] = useState(false);
 
   useEffect(() => {
     try {
+      if (calledReplace) {
+        return;
+      }
       if (isLoggedIn) {
         router.replace('/admin');
-        setRedirectPage(true);
-      } else {
-        setRedirectPage(false);
+        setCalledReplace(true);
       }
     } catch (error) {}
   }, [isLoggedIn]);
-
-  if (redirectPage) return;
 
   return (
     <>
       <Head>
         <title>{title}</title>
       </Head>
-
-      <main
-        style={{
-          margin: 'auto',
-          maxWidth: '2048px',
-          padding: '0px 5px',
-        }}
-      >
-        {children}
-      </main>
+      {!isLoggedIn && (
+        <main
+          style={{
+            margin: 'auto',
+            maxWidth: '2048px',
+            padding: '0px 5px',
+          }}
+        >
+          {children}
+        </main>
+      )}
     </>
   );
 };
