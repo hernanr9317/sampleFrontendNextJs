08695bddae473e28770e9a0ec14473baaefb9979
Hernan <hernan.r9317@gmail.com>
2023-02-04 17:07:35 -0300

    add login

--- components/AuthValidator/index.jsx
+++ components/AuthValidator/index.jsx
@@ -2,6 +2,7 @@ import {useContext} from 'react';
 import {AuthContext} from '../../context';
 import LoginPage from '../../pages/auth/login';
 import {useRouter} from 'next/router';
+import {Loading} from './../Loading/index';
 
 const withAuth = (Component) => {
   const Auth = (props) => {
@@ -12,6 +13,10 @@ const withAuth = (Component) => {
     if (isLoggedIn === false) {
       router.replace('/auth/login');
       return <LoginPage />;
+    }
+
+    if (isLoggedIn === undefined) {
+      return <Loading />;
     }
 
     // If user is logged in, return original component
