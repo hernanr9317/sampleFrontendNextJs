9e60a3e39ecb80b8d3952e8c0375ee79b484a142
Hernan <hernan.r9317@gmail.com>
2023-02-04 18:20:08 -0300

    fix tips imgs

--- components/Multimedia/TipsCol.jsx
+++ components/Multimedia/TipsCol.jsx
@@ -1,24 +1,12 @@
 import Image from 'next/image';
-import {useIsmobile} from './../../hooks/useIsMobile';
 
 export const TipsCol = ({data}) => {
   const {title, src, description, links} = data;
 
-  const isMobile = useIsmobile();
-
-  const widthImg = isMobile ? '300px' : '270px';
-  const heighImg = isMobile ? '220px' : '200px';
-
   return (
     <div className="card tipsCard">
       <div className="imgTip">
-        <Image
-          className="card-img-next"
-          width={widthImg}
-          height={heighImg}
-          layout="responsive"
-          src={src}
-        />
+        <Image className="card-img-next" src={src} />
       </div>
       <div className="card-body">
         <h4 className="card-title h5 h4-sm">{title}</h4>
