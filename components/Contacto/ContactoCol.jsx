3fa62399a90534652a263108d25c8ce4280a736c
Hernan <hernan.r9317@gmail.com>
2023-03-03 18:37:43 -0300

    add alt

--- components/Contacto/ContactoCol.jsx
+++ components/Contacto/ContactoCol.jsx
@@ -4,7 +4,7 @@ export const ContactoCol = ({icon, text}) => {
   return (
     <div className="contact-element">
       <div className="contacto-img">
-        <Image src={icon} height={50} width={50} />
+        <Image src={icon} height={50} width={50} alt={text} />
       </div>
       <div className="contacto-text">
         <h6>{text}</h6>
