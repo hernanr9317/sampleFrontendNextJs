97f7ad6b924d9ae46f371ac44bfb339113795dc7
hernanr9317 <78802962+hernanr9317@users.noreply.github.com>
2023-04-05 08:32:11 -0300

    remove head comment

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head>
-        {/* <meta http-equiv="Content-Security-Policy"
-            content="default-src https://e-seguridad.chaco.gob.ar/;" /> */}
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
