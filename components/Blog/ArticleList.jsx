f6c50c5c1e9a0ecb744e9f12fad6d9247431c106
Hernan <hernan.r9317@gmail.com>
2023-06-23 17:02:34 -0300

    fix card link

--- components/Blog/ArticleList.jsx
+++ components/Blog/ArticleList.jsx
@@ -1,19 +1,14 @@
 import {useContext} from 'react';
-import {useRouter} from 'next/router';
+import Link from 'next/link';
 import dayjs from 'dayjs';
 import {ChangeDataContext} from './../../context/changeData/ChangeDataContext';
 
 export const ArticleList = () => {
-  const router = useRouter();
   const {products} = useContext(ChangeDataContext);
 
   const filterCategory = products?.productos?.filter(
     (element) => element?.categoria?.nombre === 'NOTA',
   );
-
-  const vewArticle = (article) => {
-    router.push(`/blog/${[article.pathname]}`);
-  };
 
   return (
     <div className="articleList">
@@ -22,27 +17,27 @@ export const ArticleList = () => {
       </header>
       <div className="band">
         {filterCategory?.map((article, index) => (
-          <div
-            className={`item-${index + 1}`}
-            key={index}
-            onClick={() => vewArticle(article)}
-          >
-            <div className="card">
-              <div
-                className="thumb"
-                style={{
-                  backgroundImage: `url(${
-                    article.otherImgs[0] ||
-                    'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
-                  })`,
-                }}
-              ></div>
-              <article>
-                <h1>{article.nombre}</h1>
-                {article?.subtitle !== '' && <p>{article.subtitle}</p>}
-                <span>{dayjs(article.createdAt).format('DD/MM/YYYY')}</span>
-              </article>
-            </div>
+          <div className={`item-${index + 1}`} key={index}>
+            <Link href={`/blog/${[article.pathname]}`}>
+              <a>
+                <div className="card">
+                  <div
+                    className="thumb"
+                    style={{
+                      backgroundImage: `url(${
+                        article.otherImgs[0] ||
+                        'https://images.pexels.com/photos/2312369/pexels-photo-2312369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+                      })`,
+                    }}
+                  ></div>
+                  <article>
+                    <h1>{article.nombre}</h1>
+                    {article?.subtitle !== '' && <p>{article.subtitle}</p>}
+                    <span>{dayjs(article.createdAt).format('DD/MM/YYYY')}</span>
+                  </article>
+                </div>
+              </a>
+            </Link>
           </div>
         ))}
       </div>
