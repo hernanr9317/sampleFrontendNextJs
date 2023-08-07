805b60b6a36012c5df9461102380cff9ccf2b807
Hernan <hernan.r9317@gmail.com>
2023-08-07 15:38:13 -0300

    update info cards styles

--- components/Info/InfoCol.jsx
+++ components/Info/InfoCol.jsx
@@ -1,13 +1,8 @@
-import Image from 'next/image';
 import Link from 'next/link';
-import {useRouter} from 'next/router';
-import {Card, Col} from 'react-bootstrap';
 
 export const InfoCol = ({title, text, src, link}) => {
-  const router = useRouter();
   return (
     <>
-      {/* <Col> */}
       <article className="card">
         <img
           className="card__background"
@@ -26,32 +21,6 @@ export const InfoCol = ({title, text, src, link}) => {
           </Link>
         </div>
       </article>
-      {/* <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            marginBottom: '10px',
-            cursor: 'pointer',
-          }}
-          onClick={() => router.push(link)}
-        >
-          <Card style={{width: '18rem'}}>
-            <Image
-              variant="top"
-              src={src}
-              alt={title}
-              style={{aspectRatio: '16 / 9'}}
-            />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text>{text}</Card.Text>
-              <Link href={link} className="btn btn-primary">
-                <a className="btn btn-primary">Más información</a>
-              </Link>
-            </Card.Body>
-          </Card>
-        </div> */}
-      {/* </Col> */}
     </>
   );
 };
