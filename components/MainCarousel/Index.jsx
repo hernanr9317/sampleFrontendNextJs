eb61eefd86cc4224605ad2a495365697348dd949
Hernan <hernan.r9317@gmail.com>
2023-12-09 11:10:55 -0300

    fix carousel styles

--- components/MainCarousel/Index.jsx
+++ components/MainCarousel/Index.jsx
@@ -3,6 +3,9 @@ import {Carousel} from 'react-bootstrap';
 
 export const MainCarousel = ({img, img2, img3}) => {
   const slides = [img, img2, img3];
+  //TODO: APLICAR EL BLUR CON EN LAS IMAGENS DEL CARDBLOG
+  //ES POSIBLE HACER UN ELEMENTO IMAGE PARA NO REPETIR CODIGO
+  //SE VA A CORREGIR CUANDO SE ACTUALICE LA VERSION DE NEXT.JS DEL PROYECTO
 
   return (
     <Carousel className="mainCarousel">
