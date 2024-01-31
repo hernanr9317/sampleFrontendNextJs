f57b03e8f3d3d1b9d731cb1c5a2c1c4d513052e6
Hernan <hernan.r9317@gmail.com>
2024-01-31 16:55:12 -0300

    add date to article

--- pages/blog/[id].jsx
+++ pages/blog/[id].jsx
@@ -3,6 +3,12 @@ import {useRouter} from 'next/router';
 import {PublicLayout} from './../../components/layouts/PublicLayout';
 import {ChangeDataContext} from './../../context/changeData/ChangeDataContext';
 import ReadOnlyText from '../../components/Slate/TextRead';
+import dayjs from 'dayjs';
+import {es} from 'dayjs/locale/es';
+import localeData from 'dayjs/plugin/localeData';
+
+dayjs.locale('es');
+dayjs.extend(localeData);
 
 const Article = () => {
   const router = useRouter();
@@ -16,11 +22,16 @@ const Article = () => {
   const bodyDescription = findArticle?.description
     ? JSON.parse(findArticle?.description)
     : false;
+  
+  const date = dayjs(findArticle?.updatedAt)
+    .format('dddd D [de] MMMM [de] YYYY')
+    .replace(/^\w/, (c) => c.toUpperCase());
 
   const showArticle = findArticle && (
     <>
       <div className="title">
         <h2>{findArticle?.nombre}</h2>
+        <div className="date">{date}</div>
       </div>
       <div className="body">
         {bodyDescription &&
