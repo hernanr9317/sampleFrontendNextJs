863cc383559b3ce752f808875e4571be5f40ae53
Hernan <hernan.r9317@gmail.com>
2024-01-31 18:51:42 -0300

    add metatags to normativas

--- pages/normativas.jsx
+++ pages/normativas.jsx
@@ -2,10 +2,15 @@ import {PublicLayout} from '../components/layouts';
 import {Normativa} from '../components/Normativa';
 
 const Normativas = () => {
+  const url = `${process.env.NEXT_PUBLIC_HOST}/normativas`;
+
   return (
     <PublicLayout
       title={'e-seguridad - Normativas'}
       pageDescription={'Normativas de seguridad informática'}
+      url={url}
+      imageFullUrl="https://i.postimg.cc/K8jtb5HT/img3-47a48d0c.webp"
+      type="technology regulations"
     >
       <Normativa />
     </PublicLayout>
