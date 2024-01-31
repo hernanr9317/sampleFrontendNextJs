c8889d166a77600ff40d9b3885b775459e5d517a
Hernan <hernan.r9317@gmail.com>
2024-01-31 18:49:30 -0300

    add metatags to etaps

--- pages/etaps/index.jsx
+++ pages/etaps/index.jsx
@@ -2,10 +2,15 @@ import {Categorias} from '../../components/Categorias';
 import {PublicLayout} from '../../components/layouts';
 
 const Etaps = () => {
+  const url = `${process.env.NEXT_PUBLIC_HOST}/etaps`;
+
   return (
     <PublicLayout
       title={'e-seguridad - Etaps'}
       pageDescription={'Normativas de seguridad informática'}
+      url={url}
+      imageFullUrl="https://i.postimg.cc/K8jtb5HT/img3-47a48d0c.webp"
+      type="technology standards"
     >
       <div style={{maxWidth: '1400px', margin: 'auto'}}>
         <Categorias />
