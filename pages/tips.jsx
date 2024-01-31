e996f1fc4fce25f81477d7e47d76c778c4fc1e9a
Hernan <hernan.r9317@gmail.com>
2024-01-31 18:55:20 -0300

    add metatags to tips

--- pages/tips.jsx
+++ pages/tips.jsx
@@ -6,10 +6,15 @@ import slider2 from '../public/assets/carousel/escritorio.webp';
 import slider3 from '../public/assets/carousel/navegacion.webp';
 
 const Tips = () => {
+  const url = `${process.env.NEXT_PUBLIC_HOST}/tips`;
+
   return (
     <PublicLayout
       title={'e-seguridad - Tips'}
       pageDescription={'Tips de seguridad informática'}
+      url={url}
+      imageFullUrl="https://i.postimg.cc/K8jtb5HT/img3-47a48d0c.webp"
+      type="technology tips"
     >
       <MainCarousel img={slider1} img2={slider2} img3={slider3} />
       <Multimedia />
