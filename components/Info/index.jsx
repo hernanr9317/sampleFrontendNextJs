50fe10040e9339be2316283e21d05c50a7d54ca5
Hernan <hernan.r9317@gmail.com>
2024-06-26 16:30:30 -0300

    finish update landing page

--- components/Info/index.jsx
+++ components/Info/index.jsx
@@ -7,7 +7,7 @@ import normativa from '../../public/assets/info/normativa.webp';
 export const Info = () => {
   return (
     <div id="info" className="container-fluid mt-5 mb-5 infoContainer">
-      <h1 className="heading-1">Informáte</h1>
+      <h1 className="heading-1">► Informáte</h1>
       <div className="divider-1">
         {' '}
         <span></span>
